d31dc2974d123f0cb7e637083bbcc7d85226abad
ahmetcan-symbotic <aerdogan@symbotic.com>
2024-10-17 18:16:16 -0400

    fix: remove build folders

--- build/publisherodometry/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/vision/slamcore/git_codes/empty-odom-publisher/install/publisherodometry'
